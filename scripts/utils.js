0f1cf522ab98eb9b2a34aaf20b7202a030c6ad38
DrkSephy <dleonar00@citymail.cuny.edu>
2016-03-27 17:38:08 -0400

    Adding auth headers to avoid low rate limits for Github Search API

--- scripts/utils.js
+++ scripts/utils.js
@@ -3,7 +3,14 @@ var request = require('request');
 module.exports = {
   fetchJSON: function(url) {
     return new Promise((resolve, reject) => {
-      request.get(url, { 'headers': {'User-Agent': 'DrkSephy' }},
+      request.get(url,
+        {
+          'headers': {'User-Agent': 'DrkSephy' },
+          // If you don't want to get rate-limited, uncomment the following line
+          // And add your Github username and password.
+          // NOTE: Don't push to the repository with this data!
+          // 'auth': {'user': 'your-github-username', 'pass': 'your-github-password'}
+        },
       (error, response, body) => {
         if (body) {
           resolve(JSON.parse(body));
